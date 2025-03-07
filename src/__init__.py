d67d661b1170e97aaba89766ebc91835d5505f48
fraziphy <farhad.razi.1988@gmail.com>
2025-03-07 13:47:43 +0100

    Add __init__.py to src directory
    - Create empty __init__.py file in src folder
    - Enable src directory to be treated as a Python package
    - Facilitate proper importing of modules from the src directory
    - Improve project structure and module organization

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+# src/force_training_rate_network/__init__.py
+from .models import important_function, ImportantClass
+from .training import another_function
+
+__version__ = "0.1.0"  # If not using setuptools_scm
